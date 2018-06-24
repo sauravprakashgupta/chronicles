ced47afd8585fff97be26376e945a93063ea10c8
saurav gupta <iamsaurav.pr.gupta@hotmail.com>
2018-06-24 17:05:32 +0530

    listing articles
    displays all articles in the home page

--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Article
 
 # Create your views here.
 def index(request):
-    return render(request, 'articles/article_list.html')
+    articles = Article.objects.all().order_by('-date')
+    return render(request, 'articles/article_list.html',{'all_articles':articles})
+    
