2ae70469f1eaa54d5a9e3a3e71db62ae92e5467a
saurav Gupta <iamsaurav.pr.gupta@hotmail.com>
2018-06-27 23:39:58 +0530

    creating Login form and authenticating login
    Using AuthenticationForm to login to the account and redirecting article_list.html

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,6 @@ app_name = "accounts"
 
 urlpatterns = [
     url(r'^signup/$',views.signup_view, name="signup"),
+    url(r'^login/$',views.login_view, name="login"),
     # url(r'^(?P<slug>[\w-]+)/', views.article_detail, name="detail"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 # Create your views here.
 def signup_view(request):
-    formObj = UserCreationForm()
+    if request.method == "POST":
+        formObj=UserCreationForm(request.POST)
+        if formObj.is_valid():
+            formObj.save()
+            #log the user in
+            return redirect('articles:index')
+    else:
+        formObj = UserCreationForm()
     return render(request, 'accounts/signup.html',{'form':formObj})
+
+def login_view(request):
+    if request.method == "POST":
+        formObj = AuthenticationForm(data=request.POST)
+        if formObj.is_valid():
+            return redirect('articles:index')
+    else:
+        formObj = AuthenticationForm()
+    return render(request, 'accounts/login.html',{'form':formObj})
