8d61ec5180975fffb5b1033e8b3b441fa96a0c8a
saurav Gupta <iamsaurav.pr.gupta@hotmail.com>
2018-09-01 01:24:14 +0530

    .
    .

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
 
 # Create your views here.
 def signup_view(request):
     if request.method == "POST":
         formObj=UserCreationForm(request.POST)
         if formObj.is_valid():
-            formObj.save()
+            user = formObj.save()
             #log the user in
+            login(request,login)
             return redirect('articles:index')
     else:
         formObj = UserCreationForm()
@@ -17,7 +19,15 @@ def login_view(request):
     if request.method == "POST":
         formObj = AuthenticationForm(data=request.POST)
         if formObj.is_valid():
+            user = formObj.get_user()
+            login(request,user)
+            # login is defined function in django
             return redirect('articles:index')
     else:
         formObj = AuthenticationForm()
     return render(request, 'accounts/login.html',{'form':formObj})
+
+def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+        return render(request, 'accounts/login.html')
